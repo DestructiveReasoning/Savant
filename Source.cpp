994311afb2d2c5fa211b2b98b2260af45098fc1b
DestructiveReasoning <harleyw@hotmail.com>
2015-11-12 20:05:12 +0000

    Fixed comments

--- Source.cpp
+++ Source.cpp
@@ -99,7 +99,7 @@ int main(int argc, char *argv[])
 	}
 	logoFile.close();
 
-	printf("\n%s\nv0.3.3\nCopyright (C) 2015 Harley Wiltzer\nPowered by Har Wiltz's Destructive Reasoning\n", TITLE);
+	printf("\n%s\nv0.3.4\nCopyright (C) 2015 Harley Wiltzer\nPowered by Har Wiltz's Destructive Reasoning\n", TITLE);
 	printf("This free software includes exactly 0 warranties\n");
 	printf("For instructions, type \'help\'.\n\n");
 	initialize();
